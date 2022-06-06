574c8f5ffa7471953ee80912449341c09dd5a521
David Crean <david.crean@tealium.comw>
2022-06-05 19:42:33 -0700

    clean up

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
 
   return (
     <div className="App">
-        <div class="container">
+        <div Name="container">
           
           <Players 
             players={players}
